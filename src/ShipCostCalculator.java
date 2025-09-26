2ac687a2280237c6c62030cdb7dc5ef1ea093d32
lillystevens625-cloud <lillystevens625@gmail.com>
2025-09-26 17:26:18 -0400

    Initial commit

---
+++ src/ShipCostCalculator.java
@@ -0,0 +1,35 @@
+//Prompt user for item price
+// If price >= 100, shipping = 0
+// Else, shipping = 2% of price
+// Output shipping and total cost
+
+import java.util.Scanner;
+
+public class ShipCostCalculator {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter the price of the item: ");
+        double price = 0;
+        double shipping = 0;
+
+        if (in.hasNextDouble()) {
+            price = in.nextDouble();
+            in.nextLine();
+
+            if (price >= 100) {
+                shipping = 0;
+            } else {
+                shipping = price * 0.02;
+            }
+
+            double total = price + shipping;
+            System.out.println("Shipping cost: $" + shipping);
+            System.out.println("Total cost: $" + total);
+
+        } else {
+            String trash = in.nextLine();
+            System.out.println("Invalid input: " + trash);
+        }
+    }
+}
